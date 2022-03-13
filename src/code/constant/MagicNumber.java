a97b5c083267986eaa8dada4ba8fe85c07000feb
yangxcc <1434890515@qq.com>
2022-03-13 15:36:55 +0800

    image

---
+++ src/code/constant/MagicNumber.java
@@ -0,0 +1,11 @@
+package code.constant;
+
+public class MagicNumber {
+
+
+    public final static byte[] MAGIC_NUMBER_BIG_ENDIAN = new byte[]{0x1A, 0x2B, 0x3C, 0x4D};
+
+    public final static byte[] MAGIC_NUMBER_LITTLE_ENDIAN = new byte[]{0x4D, 0x3C, 0x2B, 0x1A};
+
+
+}
---
+++ src/code/utils/Endianess.java
@@ -0,0 +1,12 @@
+package code.utils;
+
+public class Endianess {
+    /**
+     * define error of endianess
+     */
+    public final static byte NO_ENDIANESS = 0x00;
+
+    public final static byte BIG_ENDIAN = 0x01;
+
+    public final static byte LITTLE_ENDIAN = 0x02;
+}
